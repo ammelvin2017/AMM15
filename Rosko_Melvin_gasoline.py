93c91bdb1cc0753ecbe016a41b4e91f2e9d11c82
wrosko <wrosko@bates.edu>
2015-04-30 11:11:13 -0400

    added one line to lab 2

--- Rosko_Melvin_gasoline.py
+++ Rosko_Melvin_gasoline.py
@@ -8,6 +8,10 @@ Description:
 #ask user to input number of gallons of gasoline
 gas_gallons = float(raw_input('Please enter the number of gallons of gasoline: '))
 
+#ask user for national average
+national_average = float(raw_input('Please enter the national average for the price of \
+                                   1 gallon of gasoline. (On 4/30/15 it was $2.58)  '))
+
 #1 gallon of gasoline produces apporoximately 19.64 pounds of carbon dioxide
 carbon_dioxide_pounds = gas_gallons * 19.64
 
@@ -20,8 +24,10 @@ crude_oil_barrels = gas_gallons / 19
 #output the number of barrels of crude oil required
 print crude_oil_barrels, 'barrels of crude oil produces approximately', gas_gallons, 'gallons of gasoline.'
 
+
 #average cost of X gallons of gasoline in the US today
-avg_cost_gas_gallons = gas_gallons * 2.653
+avg_cost_gas_gallons = gas_gallons * national_average
+
 
 #output the average cost of X barrels of gasoline
-print
+print gas_gallons,'gallons will cost $',avg_cost_gas_gallons
